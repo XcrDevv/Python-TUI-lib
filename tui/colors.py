263de8088570233393bd2cfb01fcd1a03312eedb
xcrdev <xanderdevxcr@gmail.com>
2026-03-06 22:50:16 -0500

    refactor: remove print format table

--- tui/colors.py
+++ tui/colors.py
@@ -64,16 +64,3 @@ def gray(text: str):
 
 def bold(text: str):
     return f'\033[1m{text}\033[0m'
-
-def print_format_table():
-    """
-    prints table of formatted text format options 
-    """
-    for style in range(8):
-        for fg in range(30,38):
-            s1 = ''
-            for bg in range(40,48):
-                format = ';'.join([str(style), str(fg), str(bg)])
-                s1 += '\x1b[%sm %s \x1b[0m' % (format, format)
-            print(s1)
-        print('\n')
